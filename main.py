b19687da7c59859fab8b2b6c81f8339e28eb6a37
fabiokusaba <fabinhokb44@gmail.com>
2024-07-30 19:52:33 -0300

    flask na prática: paths, templates e cookies

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, make_response
+from markupsafe import escape
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1> Ecommerce FastBuy</h1>"
+    return render_template('index.html')
 
 @app.route("/sobre")
 def sobre():
@@ -13,3 +16,15 @@ def sobre():
 @app.route("/sobre/privacidade")
 def sobre_privacidade():
     return "<h4> Nosso site segue as Leis Gerais de Proteção aos Dados, seus dados estão totalmente seguros.</h4>"
+
+@app.route("/user/<username>")
+def username(username):
+    cookie = make_response("<h2>cookie criado</h2>")
+    cookie.set_cookie('username', username)
+    return cookie
+
+@app.route("/user2/")
+@app.route("/user2/<username>")
+def username2(username=None):
+    cookie_username = request.cookies.get('username')
+    return render_template('user.html', username=username, cookie_username=cookie_username)
