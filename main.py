3de59cdc4e3a4c8c51fb40b0340f2de2eaa8f924
fabiokusaba <fabinhokb44@gmail.com>
2024-08-06 20:30:09 -0300

    adicionado: criação da entidade Anuncio, capturas de erros da aplicação, CRUD Anuncio,  CRUD Categoria

--- main.py
+++ main.py
@@ -35,6 +35,24 @@ class Usuario(db.Model):
         self.endereco = endereco
 
 
+class Anuncio(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    titulo = db.Column('titulo', db.String(150))
+    descricao = db.Column('descricao', db.String(150))
+    quantidade = db.Column('quantidade', db.Integer)
+    preco = db.Column('preco', db.Numeric(10, 2))
+    categoria_id = db.Column('categoria_id', db.Integer, db.ForeignKey("categoria.id"))
+    usuario_id = db.Column('usuario_id', db.Integer, db.ForeignKey("usuario.id"))
+
+    def __init__(self, titulo, descricao, quantidade, preco, categoria_id, usuario_id):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.quantidade = quantidade
+        self.preco = preco
+        self.categoria_id = categoria_id
+        self.usuario_id = usuario_id
+
+
 class Categoria(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     nome = db.Column('nome', db.String(100))
@@ -43,9 +61,20 @@ class Categoria(db.Model):
         self.nome = nome
 
 
+@app.errorhandler(404)
+def pagina_nao_encontrada(error):
+    return render_template('error404.html')
+
+
+@app.errorhandler(500)
+def erro_interno_servidor(error):
+    return render_template('error500.html')
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route("/cadastro/usuario")
 def rota_usuario():
@@ -61,7 +90,7 @@ def cadastrar_usuario():
 @app.route("/usuario/detalhar/<int:id>")
 def buscar_usuario(id):
     usuario = Usuario.query.get(id)
-    return usuario.nome
+    return render_template('detalhar_usuario.html', usuario = usuario, titulo = "Usuário")
 
 @app.route("/usuario/editar/<int:id>", methods=['GET', 'POST'])
 def editar_usuario(id):
@@ -87,26 +116,61 @@ def remover_usuario(id):
 
 
 @app.route("/cadastro/anuncio")
-def anuncio():
-    return render_template('anuncio.html', categorias = Categoria.query.all(), titulo = "Anúncio")
+def rota_anuncio():
+    return render_template('anuncio.html', categorias = Categoria.query.all(), anuncios = Anuncio.query.all(), titulo = "Anúncio")
 
 @app.route("/anuncio/cadastrar", methods=['POST'])
 def cadastrar_anuncio():
-    return request.form
+    anuncio = Anuncio(request.form.get('titulo'), request.form.get('descricao'), request.form.get('quantidade'), request.form.get('preco'), request.form.get('categoria'), request.form.get('usuario'))
+    db.session.add(anuncio)
+    db.session.commit()
+    return redirect(url_for('rota_anuncio'))
+
+@app.route("/anuncio/detalhar/<int:id>")
+def buscar_anuncio(id):
+    anuncio = Anuncio.query.get(id)
+    return render_template('detalhar_anuncio.html', anuncio = anuncio, titulo = "Anúncio")
+
+@app.route("/anuncio/editar/<int:id>", methods=['GET', 'POST'])
+def editar_anuncio(id):
+    anuncio = Anuncio.query.get(id)
+    if request.method == 'POST':
+        anuncio.titulo = request.form.get('titulo')
+        anuncio.descricao = request.form.get('descricao')
+        anuncio.quantidade = request.form.get('quantidade')
+        anuncio.preco = request.form.get('preco')
+        anuncio.categoria_id = request.form.get('categoria')
+        anuncio.usuario_id = request.form.get('usuario')
+        db.session.add(anuncio)
+        db.session.commit()
+        return redirect(url_for('rota_anuncio'))
+
+    return render_template('editar_anuncio.html', anuncio = anuncio, categorias = Categoria.query.all(), titulo = "Anúncio")
+
+@app.route("/anuncio/remover/<int:id>")
+def remover_anuncio(id):
+    anuncio = Anuncio.query.get(id)
+    db.session.delete(anuncio)
+    db.session.commit()
+    return redirect(url_for('rota_anuncio'))
+
 
 @app.route("/anuncios/pergunta")
 def pergunta_anuncio():
     return render_template('pergunta.html', titulo = "Perguntas do Anúncio")
+
 
 @app.route("/anuncios/compra")
 def compra_anuncio():
     print('Compra realizada com sucesso!')
     return ""
 
+
 @app.route("/anuncios/favoritos")
 def anuncio_favorito():
     print('Anúncio favoritado com sucesso!')
     return ""
+
 
 @app.route("/config/categoria")
 def rota_categoria():
@@ -119,9 +183,34 @@ def cadastrar_categoria():
     db.session.commit()
     return redirect(url_for('rota_categoria'))
 
+@app.route("/categoria/detalhar/<int:id>")
+def buscar_categoria(id):
+    categoria = Categoria.query.get(id)
+    return render_template('detalhar_categoria.html', categoria = categoria, titulo = "Categoria")
+
+@app.route("/categoria/editar/<int:id>", methods=['GET', 'POST'])
+def editar_categoria(id):
+    categoria = Categoria.query.get(id)
+    if request.method == 'POST':
+        categoria.nome = request.form.get('nome')
+        db.session.add(categoria)
+        db.session.commit()
+        return redirect(url_for('rota_categoria'))
+
+    return render_template('editar_categoria.html', categoria = categoria, titulo = "Categoria")
+
+@app.route("/categoria/remover/<int:id>")
+def remover_categoria(id):
+    categoria = Categoria.query.get(id)
+    db.session.delete(categoria)
+    db.session.commit()
+    return redirect(url_for('rota_categoria'))
+
+
 @app.route("/relatorio/vendas")
 def relatorio_venda():
     return render_template('vendas.html', titulo = "Relatório de Vendas")
+
 
 @app.route("/relatorio/compras")
 def relatorio_compra():
