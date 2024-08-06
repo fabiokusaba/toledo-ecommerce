70f318e66530b663b20ad21bf4e0a83b60073d59
fabiokusaba <fabinhokb44@gmail.com>
2024-08-05 21:06:33 -0300

    adicionado: mapeamento ORM da entidade Usuario com SQLAlchemy

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask import render_template
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
 
 app = Flask(__name__)
+
+user = 'root'
+password = '@Lucia25037795'
+host = 'localhost'
+port = '3306'
+database = 'trocafacil'
+escaped_password = quote(password)
+uri = f'mysql+pymysql://{user}:{escaped_password}@{host}:{port}/{database}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    nome = db.Column('nome', db.String(150))
+    email = db.Column('email', db.String(50))
+    password = db.Column('password', db.String(10))
+    telefone = db.Column('telefone', db.String(50))
+    endereco = db.Column('endereco', db.String(100))
+
+    def __init__(self, nome, email, password, telefone, endereco):
+        self.nome = nome
+        self.email = email
+        self.password = password
+        self.telefone = telefone
+        self.endereco = endereco
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/cadastro/usuario")
-def usuario():
-    return render_template('usuario.html', titulo="Cadastro de Usuário")
+def rota_usuario():
+    return render_template('usuario.html', usuarios = Usuario.query.all(), titulo="Usuário")
 
 @app.route("/cadastro/cadastrar_usuario", methods=['POST'])
 def cadastrar_usuario():
-    return request.form
+    usuario = Usuario(request.form.get('nome'), request.form.get('email'), request.form.get('password'), request.form.get('telefone'), request.form.get('endereco'))
+    db.session.add(usuario)
+    db.session.commit()
+    return redirect(url_for('rota_usuario'))
 
 @app.route("/cadastro/anuncio")
 def anuncio():
@@ -49,3 +83,9 @@ def relatorio_venda():
 @app.route("/relatorio/compras")
 def relatorio_compra():
     return render_template('compras.html', titulo="Relatório de Compras")
+
+
+if __name__ == 'main':
+    with app.app_context():
+        print('trocafacil')
+        db.create_all()
